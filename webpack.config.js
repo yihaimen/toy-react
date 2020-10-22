0b84ab946ecfa432d2f8f4bfda9f27dd38c01fa6
yihaimen <yihaimen@outlook.com>
2020-10-22 22:51:24 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: {
+    main: './main.js'
+  },
+  mode: 'development',
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
