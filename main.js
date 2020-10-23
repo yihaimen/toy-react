5a95093d3080eb5d04b627c5860c6090fc357389
yihaimen <yihaimen@outlook.com>
2020-10-23 22:08:00 +0800

    add: JSX原理和关键实现

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-for (const i of [1,2,3,4,5]) {
-  console.log(i);
+import { createElement, render, Component } from './toy-react';
+
+class MyComponent extends Component{
+  render() {
+    return (<div>
+      <h1>my component</h1>
+      {this.children}
+    </div>)
+  }
 }
+
+render(<MyComponent id='a' class='c'> 
+  <div>abc</div>
+  <div>efg</div>
+  <div>hyz</div>
+</MyComponent>, document.body);
