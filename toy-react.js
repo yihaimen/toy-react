b3a8f09e621a4d4c2e35153fdfda2dcd819d9343
yihaimen <yihaimen@outlook.com>
2020-10-26 22:35:31 +0800

    add: tic tac toe

--- toy-react.js
+++ toy-react.js
@@ -5,7 +5,15 @@ class ElementWrapper {
     this.root = document.createElement(type);
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value);
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', value);
+      } else {
+        this.root.setAttribute(name, value);
+      }
+    }
   }
   appendChild(component) {
     let range = document.createRange();
@@ -38,11 +46,7 @@ export class Component {
     this._range = null;
   }
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
-    } else {
-      this.props[name] = value;
-    }
+    this.props[name] = value;
   }
   appendChild(component) {
     this.children.push(component);
@@ -52,12 +56,36 @@ export class Component {
     this.render()[RENDER_TO_DOM](range);
   }
   rerender() {
-    this._range.deleteContents();
-    this[RENDER_TO_DOM](this._range);
+    let oldRange = this._range;
+    let range = document.createRange();
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    range.deleteContents();
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
   }
-  // setState(newState) {
-  //   let merge = 
-  // }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState;
+      this.rerender();
+      return;
+    }
+
+    let merge = (oldState, newState) => {
+      for (const p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p];
+        } else {
+          merge(oldState[p], newState[p]);
+        }
+      }
+    };
+
+    merge(this.state, newState);
+    this.rerender();
+  }
 }
 
 export function createElement(type, attributes, ...children) {
@@ -76,6 +104,11 @@ export function createElement(type, attributes, ...children) {
       if (typeof child === "string") {
         child = new TextWrapper(child);
       }
+
+      if (child === null) {
+        continue;
+      }
+
       if (typeof child === "object" && child instanceof Array) {
         insertChildren(child);
       } else {
